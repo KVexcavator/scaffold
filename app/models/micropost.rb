c8071c599e94d14dbc8a06a48caedbda66029108
KVexcavator <str_token@mail.ru>
2018-11-06 16:57:54 +0600

    add validation user,micropost  models

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 50}
+  validates :content, length: { maximum: 50}, presence: true
+  validates :users_id,  presence: true
 end
